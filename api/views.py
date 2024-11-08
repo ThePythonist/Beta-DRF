afb944d868a6ba5035b3ca5ae73e89e917d1f7b4
Arash <awrash.sn@gmail.com>
2024-11-08 14:36:21 +0330

    Almost done. Need to scrape now

--- api/views.py
+++ api/views.py
@@ -23,23 +23,7 @@ class BetaView(ListCreateAPIView):
         end_date = self.request.GET.get('end_date', )
 
         if stock_name and start_date and end_date:
-            #     if not os.path.exists(f"api/شاخص كل/شاخص كل-{start_date}-{end_date}.xlsx"):
-            #         fetch_stock_historical_data('شاخص كل', start_date, end_date)
-            #     else:
-            #         make_log('info', f'market-history already exists for {start_date}-{end_date}')
-            #
-            #     if not os.path.exists(f"api/{stock_name}/{stock_name}-{start_date}-{end_date}.xlsx"):
-            #         fetch_stock_historical_data(stock_name, start_date, end_date)
-            #     else:
-            #         make_log('info', f'stock-history for {stock_name} already exists for {start_date}-{end_date}')
-            #
-            #     beta = calculate_beta(stock_name, start_date, end_date)
-
-            # print(f'Beta of {stock_name} in {start_date}-{end_date} is {calculate_beta(stock_name, start_date, end_date)}')
-
-            # Prepare the filter criteria
             filters = Q()
-
             if stock_name:
                 filters &= Q(stock_name__icontains=stock_name)
 
@@ -55,7 +39,35 @@ class BetaView(ListCreateAPIView):
             if len(queryset) == 1:
                 return queryset
             elif len(queryset) == 0:  # Beta doesnt exists
-                return Beta.objects.none()
+                # return Beta.objects.none()
+
+                # if not os.path.exists(f"api/شاخص كل/شاخص كل-{start_date}-{end_date}.xlsx"):
+                #     fetch_stock_historical_data('شاخص كل', start_date, end_date)
+                # else:
+                #     make_log('info', f'market-history already exists for {start_date}-{end_date}')
+                #
+                # if not os.path.exists(f"api/{stock_name}/{stock_name}-{start_date}-{end_date}.xlsx"):
+                #     fetch_stock_historical_data(stock_name, start_date, end_date)
+                # else:
+                #     make_log('info', f'stock-history for {stock_name} already exists for {start_date}-{end_date}')
+                #
+                # beta = calculate_beta(stock_name, start_date, end_date)
+                #
+                # print(
+                #     f'Beta of {stock_name} in {start_date}-{end_date} is {calculate_beta(stock_name, start_date, end_date)}')
+
+                beta = 1
+                market_index = 1
+
+                new_data = Beta.objects.create(
+                    stock_name=stock_name,
+                    start_date=start_date,
+                    end_date=end_date,
+                    value=beta,
+                    market_index=market_index,
+                )
+
+                return Beta.objects.filter(id=new_data.id)
             else:
                 print("There was a problem with the queryset")
         else:
